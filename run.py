0b15d653c5f527747d94e3c0d40ddc87e4658a36
ION606 <orenni@rpi.edu>
2025-11-12 10:37:09 -0500

    added restart policy

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ app = create_app()
 
 if __name__ == "__main__":
     os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
+    print(f"listening on port {config.Config.PORT}")
+
     with app.app_context():
         db.create_all()
         serve(app, host="0.0.0.0", port=config.Config.PORT)
